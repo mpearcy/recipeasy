1e6b36817283f598120fdc92a295ccec6d26d8b5
mpearcy <matthew.pearcy@gmail.com>
2019-03-31 11:34:39 -0600

    added sample screenshot

--- app/src/main/java/ca/mattpearcy/recipeasy/Module.kt
+++ app/src/main/java/ca/mattpearcy/recipeasy/Module.kt
@@ -7,7 +7,6 @@ import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-// declared ViewModel using the viewModel keyword
 val diModule : Module = module {
     single { RecipePuppyApi.create()}
     viewModel { IngredientSearchViewModel(get()) }
